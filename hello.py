5e7566e4d5e6466a8109c96e6d68d9598afbbbf9
Queverity <pryor.baird@ucas-edu.net>
2024-09-06 20:34:31 +0000

    made changes to name printers

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
-name = input("What is your name?")
-print("Hello " + name + ".")
+# | CHECK THIS OUT
+# V ITS A VARIABLE
+IM_A_VARIABLE = input("What is your name?")
+print("Hello " + IM_A_VARIABLE + ".")
--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-    char name[30] = "";
+    char name[30];
     printf("What is your name? \n");
     scanf("%s",name);
     printf("Hello %s", name);
