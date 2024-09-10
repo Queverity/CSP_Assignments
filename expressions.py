d8884c94b611cc74a1f089ad597eb8773e7b71b2
Queverity <pryor.baird@ucas-edu.net>
2024-09-10 19:44:00 +0000

    made expressions

---
+++ expressions.py
@@ -0,0 +1,26 @@
+questionOne = 7-24/8*4+6
+print(questionOne)
+
+questionTwo = 18/3-7+2*5
+print(questionTwo)
+
+questionThree = 6*4/12+72/8-9
+print(questionThree)
+
+questionFour = (17-6/2)+4*3
+print(questionFour)
+
+questionFive = 0-2*(1*4-2/2) + (6+2-3)
+print(questionFive)
+
+questionSix = 0-1*((3-4*7)/5)-2*24/6
+print(questionSix)
+
+questionSeven = (3*5**2/15)-(5-2**2)
+print(questionSeven)
+
+questionEight = (1**4*2**2+3**3)-2**5/4
+print(questionEight)
+
+questionNine = (22/2-2*5)**2+(4-6/6)**2
+print(questionNine)
