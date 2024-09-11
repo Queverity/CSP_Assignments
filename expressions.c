34491026a8d238af830f9e8d9701d5d23d63dee6
Queverity <pryor.baird@ucas-edu.net>
2024-09-11 04:05:35 +0000

    made C expressions

---
+++ expressions.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void){
+int questionOne = 7-24/8*4+6;
+printf("%d\n",questionOne);
+
+int questionTwo = 18/3-7+2*5;
+printf("%d\n",questionTwo);
+
+int questionThree = 6*4/12+72/8-9;
+printf("%d\n",questionThree);
+
+int questionFour = (17-6/2)+4*3;
+printf("%d\n",questionFour);
+
+int questionFive = 0-2*(1*4-2/2) + (6+2-3);
+printf("%d\n",questionFive);
+
+int questionSix = 0-1*((3-4*7)/5)-2*24/6;
+printf("%d\n",questionSix);
+
+int questionSeven = (3*(5*5)/15)-(5-(2*2));
+printf("%d\n",questionSeven);
+
+int questionEight = ((1*1*1*1)*(2*2)+(3*3*3))-(2*2*2*2*2)/4;
+printf("%d\n",questionEight);
+
+int questionNine = (22/2-2*5)*(22/2-2*5)+(4-6/6)*(4-6/6);
+printf("%d\n",questionNine);
+return 0;
+
+}
