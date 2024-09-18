1775e2e6eda1787d1e8c6f281141c93617f07bbf
Queverity <pryor.baird@ucas-edu.net>
2024-09-18 19:06:57 +0000

    made silly sentences

---
+++ silly.py
@@ -0,0 +1,4 @@
+verb = input("Give me a verb.")
+noun = input("Give me a noun.")
+food = input("Give me a type of food.")
+print("The man ate his " + verb + " " + food + " on the plate with a " + noun + ".")
