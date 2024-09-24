c9bfe31056554fff1d764879e4773309ebd8eecd
Queverity <pryor.baird@ucas-edu.net>
2024-09-24 19:11:22 +0000

    improved python financial calculator

--- financialcalc.py
+++ financialcalc.py
@@ -18,17 +18,29 @@ commutePercent = (transportation/monthlyIncome)
 
 savings = (monthlyIncome*0.15)
 
-print("You should put " + str(savings) + " dollars into your savings account. This is 15 percent of your monthly income.")
-
-print("Rent is " + str(rentPercent) + " percent of your monthly income.")
-
-print("Utilities are " + str(utilPercent) + " percent of your monthly income.")
-
-print("Groceries are " + str(grocPercent) + " percent of your monthly income.")
-
-print("Commute is " + str(commutePercent) + " percent of your monthly income.")
-
 accountAfter = int((monthlyIncome-(rent+utilities+groceries+transportation+savings)))
 
-print("You have " + str(accountAfter) + " dollars left from your monthly income. Have fun!")
+expenses = int(rent+utilities+transportation+groceries+savings)
+
+def percent(type, amount):
+    per = amount/monthlyIncome*100
+    print(f"Your {type} is {per}% income.")
+
+print(f"You should put " + str(savings) + " dollars into your savings account. This is 15 percent of your monthly income.")
+
+print(f"You have " + str(accountAfter) + " dollars left from your monthly income. Have fun!")
+
+print(f"Your monthly income is $" + str(monthlyIncome) + ".")
+
+print(f"Your expenses are $" + str(expenses) + ".")
+
+percent("rent", rent)
+
+percent("groceries", groceries)
+
+percent("utilities", utilities)
+
+percent("commute", transportation)
+
+
 
