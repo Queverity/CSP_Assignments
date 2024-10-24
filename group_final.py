3d4287f1105806ba601f78c625233489f15cf264
Queverity <pryor.baird@ucas-edu.net>
2024-10-24 19:31:09 +0000

    finally finsihed, slay queen yass lets go this is skibidi
    Co-authored-by: AngeredBarbeque <AngeredBarbeque@users.noreply.github.com>
    Co-authored-by: Deanrobert-dell <Deanrobert-dell@users.noreply.github.com>

--- group_final.py
+++ group_final.py
@@ -1,84 +1,74 @@
 import random
-#Hangman, by Clayton Baird, Nick L., Dean peterson, Dean Peterson, and Wakefield Batty
-#comments bad
-
+#Hangman, by Clayton Baird, Nick Larsen, Dean Peterson,and Wakefield Batty
 
 
 #Make a list of words - Everyone
-words = ['computer', 'dentures', 'science','moles','principles','rizzler','pryor','crunchy','black','hole','dictionary','dinosaur','defenestration','pnemonoultramicroscopicpsilicovolcanoconiosis','teeth','blooket','nick','dean','wakefield','deepfriedstickofbutter']
-word = words[random.randint(0,19)]
-wordLength = len(word)
-lettersGuessed = []
-lettersGuessedStr = '' 
-lettersFailed = []
-#lettersFailedStr = ''
-tries = 7
-incorrectGuesses = 0
-for i in range(wordLength):
-    lettersGuessed.insert(i, "_")
-playing = True
 
-#Allow people to choose to play again or not - Pryor
-def playingAgain(playing):
-    playAgain = input("Would you like to play again? Yes/No")
-    if playAgain == "Yes":
-        word = words[random.randint(0,19)] #dean
-        wordLength = len(word)
-        lettersGuessed = []
-        lettersGuessedStr = '' 
-        lettersFailed = []
-        tries = 7
-        incorrectGuesses = 0
-        for i in range(wordLength):
-            lettersGuessed.insert(i, "_")
-        playing = True
-    else:
-        print("Goodbye! Please come back, I get lonely. I might commit  several unforgivable violent war crimes if you don't.")
-        playing = False
-# Set amount of tries - Dean
-print("Hello! Welcome to hangman. A Word has been chosen at random.\nBy the way, I haven't commited any war crimes.\nYet.")
-print(word)
-while playing:
+def main():
+    #edited↓words logicicizing - dean
+    words = ['computer', 'dentures', 'science','moles','principles','rizzler','pryor','crunchy','black','hole','dictionary','dinosaur','defenestration','pnemonoultramicroscopicpsilicovolcanoconiosis','teeth','blooket','nick','dean','wakefield','deepfriedstickofbutter','bullcrap']
     #Make something to choose words - Everyone
-    i = 0
-    listWord = []
-    #Allow people to input letters - Nicholas
-    guess = input("Please guess a letter.")
-    if guess in lettersFailed or guess in lettersGuessed:
-        print("Sorry, you already tried that.")
-        continue
-    if len(guess) > 1:
-        print("Please enter one letter.\nYou bug. You small little baby buffoon.\nYou fool. You court jester. \nYou worthless piece of scrap. \nYou son of a ingrown hamsters toenail. \n Your mother was a hamster and your father smelled of elderberries!")
-        continue
-    correct = False
-    listWord = list(word)
-    for item in listWord:       
-        #Fill gaps in word with correct letters - Nicholas
-        if guess == listWord[i]:
-            lettersGuessed[i] = guess
-            lettersGuessedStr.join(lettersGuessed)
-            correct = True
-        i += 1
-    if correct != True:
-        lettersFailed.insert(i, guess)
-        tries -= 1
-        incorrectGuesses += 1
-    #Print out incorrect letters - Pryor
-    print("Your current incorrect letters are: ")
-    print(lettersFailed)
-    print("Your current correct letters are:")
-    print(lettersGuessed)
-    print("You have " + str(tries) +  " tries left.")
+    word = words[random.randint(0,20)]
+    wordLength = len(word)
+    lettersGuessed = []
+    lettersGuessedStr = '' 
+    lettersFailed = []
+    # Set amount of tries - Dean
+    tries = 7
+    incorrectGuesses = 0
+    for i in range(wordLength):
+        lettersGuessed.insert(i, "_")
     
-            
-    
-    
-    #Show if someone has won or not -Wakefield
-    if lettersGuessed == listWord:
-        print("You won! \n The word was " + word + ".")  
-        playingAgain(playing) 
-    elif incorrectGuesses >= 7:
-        print("You just hanged a man. He is now dead. \n There is blood on your hands now. His family is miserable without him, and they have no food to eat because he bought all of it. Anyways, the word was " + word + ".")
-        playingAgain(playing)
-    if playing != True:
-        break
+    print("Hello! Welcome to hangman. A Word has been chosen at random.\nBy the way, I haven't commited any war crimes.\nYet.")
+    while True:
+        i = 0
+        listWord = []
+        #Allow people to input letters - Nicholas
+        guess = input("Please guess a letter.")
+        if guess in lettersFailed or guess in lettersGuessed:
+            print("Sorry, you already tried that.")
+            continue
+        if len(guess) > 1:
+            print("Please enter one letter.\nYou bug. You small little baby buffoon.\nYou fool. You court jester. \nYou worthless piece of scrap. \nYou son of a ingrown hamsters toenail. \n Your mother was a hamster and your father smelled of elderberries!")
+            continue
+        correct = False
+        listWord = list(word)
+        for item in listWord:       
+            #Fill gaps in word with correct letters - Nicholas
+            if guess == listWord[i]:
+                lettersGuessed[i] = guess
+                lettersGuessedStr.join(lettersGuessed)
+                correct = True
+            i += 1
+        if correct != True:
+            lettersFailed.insert(i, guess)
+            tries -= 1
+            incorrectGuesses += 1
+        #Print out incorrect letters - Pryor
+        def letterPrint(lettersFailed,lettersGuessed,tries):
+            print("Your current incorrect letters are: ")
+            print(lettersFailed)
+            print("Your current correct letters are:")
+            print(lettersGuessed)
+            print("You have " + str(tries) +  " tries left.")
+        letterPrint(lettersFailed,lettersGuessed,tries)
+        
+        
+        #Show if someone has won or not -Wakefield
+        if lettersGuessed == listWord:
+            print("You won!")
+            playingAgain = input("Would you like to play again? Y/N:")
+            if playingAgain == "Yes":
+                main()
+            else:
+                print("Goodbye! Please come back, I get lonely. I might commit several unforgivable violent war crimes if you don't.")
+                exit()
+        elif incorrectGuesses >= 7:
+            print("You just hanged a man. He is now dead. \n There is blood on your hands now. His family is miserable without him, and they have no food to eat because he bought all of it. Anyways, the word was " + word + ".")
+            playingAgain = input("Would you like to play again? Y/N:")
+            if playingAgain == "Y":
+                main()
+            else:
+                print("Goodbye! Please come back, I get lonely. I might commit several unforgivable violent war crimes if you don't.")
+                exit()
+main()
